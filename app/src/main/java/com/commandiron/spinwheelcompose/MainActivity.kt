bc08ffeeb2b1a84ab92ba5efef8510d71ee86c86
Emir Demirli <commandiron@gmail.com>
2022-08-17 16:12:41 +0300

    add gif.

--- app/src/main/java/com/commandiron/spinwheelcompose/MainActivity.kt
+++ app/src/main/java/com/commandiron/spinwheelcompose/MainActivity.kt
@@ -42,7 +42,7 @@ class MainActivity : ComponentActivity() {
                             isSpinning = !isSpinning
                         },
                         onFinish = {
-                            isSpinning = !isSpinning
+                            isSpinning = false
                         }
                     )
                 }
