e9fe3e10d0dde4793086a40edb5f6d67998e7570
Emir Demirli <commandiron@gmail.com>
2022-08-22 00:27:28 +0300

    .

--- app/src/main/java/com/commandiron/spinwheelcompose/MainActivity.kt
+++ app/src/main/java/com/commandiron/spinwheelcompose/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
@@ -38,64 +39,65 @@ class MainActivity : ComponentActivity() {
                     horizontalAlignment = Alignment.CenterHorizontally,
                     verticalArrangement = Arrangement.Center
                 ) {
-//                    val textList by remember {
-//                        mutableStateOf(
-//                            listOf("Pie 1", "Pie 2", "Pie 3", "Pie 4", "Pie 5", "Pie 6", "Pie 7", "Pie 8")
-//                        )
-//                    }
-//                    DefaultSpinWheel(isSpinning = true){ pieIndex ->
-//                        Text(text = textList[pieIndex])
-//                    }
-
-                    val iconList by remember {
+                    CircularProgressIndicator()
+                    val textList by remember {
                         mutableStateOf(
-                            listOf(
-                                Icons.Default.Star,
-                                Icons.Default.Star,
-                                Icons.Default.Star,
-                                Icons.Default.Star,
-                            )
+                            listOf("Pie 1", "Pie 2", "Pie 3", "Pie 4", "Pie 5", "Pie 6", "Pie 7", "Pie 8")
                         )
                     }
-                    var isSpinning by remember { mutableStateOf(false)}
-                    repeat(3){
-                        DefaultSpinWheel(
-                            dimensions = SpinWheelDefaults.spinWheelDimensions(
-                                spinWheelSize = 180.dp,
-                                frameWidth = 20.dp,
-                                selectorWidth = 10.dp
-                            ),
-                            colors = SpinWheelDefaults.spinWheelColors(
-                                frameColor = Color(0xFF403d39),
-                                dividerColor = Color(0xFFfffcf2),
-                                selectorColor = Color(0xFFdc0073),
-                                pieColors = listOf(
-                                    Color(0xFFdabfff),
-                                    Color(0xFF907ad6),
-                                    Color(0xFF4f518c),
-                                    Color(0xFF2c2a4a)
-                                )
-                            ),
-                            animationAttr = SpinWheelDefaults.spinWheelAnimationAttr(
-                                pieCount = 4,
-                                durationMillis = 4000,
-                                delayMillis = 200,
-                                rotationPerSecond = 2f,
-                                easing = LinearOutSlowInEasing,
-                                startDegree = 90f
-                            ),
-                            isSpinning = isSpinning,
-                            onClick = { isSpinning = !isSpinning },
-                            onFinish = { isSpinning = false }
-                        ){ pieIndex ->
-                            Icon(
-                                imageVector = iconList[pieIndex],
-                                tint = Color.White,
-                                contentDescription = null
-                            )
-                        }
-                        Spacer(modifier = Modifier.height(32.dp))
+                    DefaultSpinWheel(isSpinning = true){ pieIndex ->
+                        Text(text = textList[pieIndex])
                     }
+
+//                    val iconList by remember {
+//                        mutableStateOf(
+//                            listOf(
+//                                Icons.Default.Star,
+//                                Icons.Default.Star,
+//                                Icons.Default.Star,
+//                                Icons.Default.Star,
+//                            )
+//                        )
+//                    }
+//                    var isSpinning by remember { mutableStateOf(false)}
+//                    repeat(3){
+//                        DefaultSpinWheel(
+//                            dimensions = SpinWheelDefaults.spinWheelDimensions(
+//                                spinWheelSize = 180.dp,
+//                                frameWidth = 20.dp,
+//                                selectorWidth = 10.dp
+//                            ),
+//                            colors = SpinWheelDefaults.spinWheelColors(
+//                                frameColor = Color(0xFF403d39),
+//                                dividerColor = Color(0xFFfffcf2),
+//                                selectorColor = Color(0xFFdc0073),
+//                                pieColors = listOf(
+//                                    Color(0xFFdabfff),
+//                                    Color(0xFF907ad6),
+//                                    Color(0xFF4f518c),
+//                                    Color(0xFF2c2a4a)
+//                                )
+//                            ),
+//                            animationAttr = SpinWheelDefaults.spinWheelAnimationAttr(
+//                                pieCount = 4,
+//                                durationMillis = 4000,
+//                                delayMillis = 200,
+//                                rotationPerSecond = 2f,
+//                                easing = LinearOutSlowInEasing,
+//                                startDegree = 90f
+//                            ),
+//                            isSpinning = isSpinning,
+//                            onClick = { isSpinning = !isSpinning },
+//                            onFinish = { isSpinning = false }
+//                        ){ pieIndex ->
+//                            Icon(
+//                                imageVector = iconList[pieIndex],
+//                                tint = Color.White,
+//                                contentDescription = null
+//                            )
+//                        }
+//                        Spacer(modifier = Modifier.height(32.dp))
+//                    }
                 }
             }
         }
