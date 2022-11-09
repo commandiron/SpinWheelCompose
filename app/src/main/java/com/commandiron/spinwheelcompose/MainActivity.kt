5896fc494235db152bab1ccc015b865795025bf5
Emir Demirli <commandiron@gmail.com>
2022-11-09 19:53:47 +0300

    .

--- app/src/main/java/com/commandiron/spinwheelcompose/MainActivity.kt
+++ app/src/main/java/com/commandiron/spinwheelcompose/MainActivity.kt
@@ -36,70 +36,70 @@ class MainActivity : ComponentActivity() {
                     horizontalAlignment = Alignment.CenterHorizontally,
                     verticalArrangement = Arrangement.Center
                 ) {
-//                    val textList by remember {
-//                        mutableStateOf(
-//                            listOf("Pie 1", "Pie 2", "Pie 3", "Pie 4", "Pie 5", "Pie 6", "Pie 7", "Pie 8")
-//                        )
-//                    }
-//
-//                    val state = rememberSpinWheelState()
-//                    val scope = rememberCoroutineScope()
-
-//                    SpinWheel(
-//                        state = state,
-//                        onClick = { scope.launch { state.animate() } }
-//                    ){ pieIndex ->
-//                        Text(text = textList[pieIndex])
-//                    }
-
-                    val iconList by remember {
+                    val textList by remember {
                         mutableStateOf(
-                            listOf(
-                                Icons.Default.Star,
-                                Icons.Default.Star,
-                                Icons.Default.Star,
-                                Icons.Default.Star,
-                            )
+                            listOf("Pie 1", "Pie 2", "Pie 3", "Pie 4", "Pie 5", "Pie 6", "Pie 7", "Pie 8")
                         )
                     }
 
-                    repeat(3){
-                        SpinWheel(
-                            state = rememberSpinWheelState(
-                                pieCount = 4,
-                                durationMillis = 20000,
-                                delayMillis = 200,
-                                rotationPerSecond = 2f,
-                                easing = LinearOutSlowInEasing,
-                                startDegree = 90f,
-                                resultDegree = 212f,
-                                autoSpinDelay = 0
-                            ),
-                            dimensions = SpinWheelDefaults.spinWheelDimensions(
-                                spinWheelSize = 180.dp,
-                                frameWidth = 20.dp,
-                                selectorWidth = 10.dp
-                            ),
-                            colors = SpinWheelDefaults.spinWheelColors(
-                                frameColor = Color(0xFF403d39),
-                                dividerColor = Color(0xFFfffcf2),
-                                selectorColor = Color(0xFFdc0073),
-                                pieColors = listOf(
-                                    Color(0xFFdabfff),
-                                    Color(0xFF907ad6),
-                                    Color(0xFF4f518c),
-                                    Color(0xFF2c2a4a)
-                                )
-                            )
-                        ){ pieIndex ->
-                            Icon(
-                                imageVector = iconList[pieIndex],
-                                tint = Color.White,
-                                contentDescription = null
-                            )
-                        }
-                        Spacer(modifier = Modifier.height(32.dp))
+                    val state = rememberSpinWheelState()
+                    val scope = rememberCoroutineScope()
+
+                    SpinWheel(
+                        state = state,
+                        onClick = { scope.launch { state.animate() } }
+                    ){ pieIndex ->
+                        Text(text = textList[pieIndex])
                     }
+
+//                    val iconList by remember {
+//                        mutableStateOf(
+//                            listOf(
+//                                Icons.Default.Star,
+//                                Icons.Default.Star,
+//                                Icons.Default.Star,
+//                                Icons.Default.Star,
+//                            )
+//                        )
+//                    }
+//
+//                    repeat(3){
+//                        SpinWheel(
+//                            state = rememberSpinWheelState(
+//                                pieCount = 4,
+//                                durationMillis = 20000,
+//                                delayMillis = 200,
+//                                rotationPerSecond = 2f,
+//                                easing = LinearOutSlowInEasing,
+//                                startDegree = 90f,
+//                                resultDegree = 212f,
+//                                autoSpinDelay = 0
+//                            ),
+//                            dimensions = SpinWheelDefaults.spinWheelDimensions(
+//                                spinWheelSize = 180.dp,
+//                                frameWidth = 20.dp,
+//                                selectorWidth = 10.dp
+//                            ),
+//                            colors = SpinWheelDefaults.spinWheelColors(
+//                                frameColor = Color(0xFF403d39),
+//                                dividerColor = Color(0xFFfffcf2),
+//                                selectorColor = Color(0xFFdc0073),
+//                                pieColors = listOf(
+//                                    Color(0xFFdabfff),
+//                                    Color(0xFF907ad6),
+//                                    Color(0xFF4f518c),
+//                                    Color(0xFF2c2a4a)
+//                                )
+//                            )
+//                        ){ pieIndex ->
+//                            Icon(
+//                                imageVector = iconList[pieIndex],
+//                                tint = Color.White,
+//                                contentDescription = null
+//                            )
+//                        }
+//                        Spacer(modifier = Modifier.height(32.dp))
+//                    }
                 }
             }
         }
